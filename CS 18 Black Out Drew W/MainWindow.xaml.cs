c6d949a735e76081e2ccb6d59001ac216a916a91
Drewwheeler2 <70332740+Drewwheeler2@users.noreply.github.com>
2020-12-04 10:54:11 -0600

    Not done: Made changes
    12/4/2020

---
+++ CS 18 Black Out Drew W/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CS_18_Black_Out_Drew_W
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Calendar_3_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
