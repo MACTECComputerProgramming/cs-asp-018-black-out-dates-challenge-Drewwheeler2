f91e82f67a0d24b5365b3d8b0623891708fcde93
Drewwheeler2 <70332740+Drewwheeler2@users.noreply.github.com>
2020-12-08 10:45:36 -0600

    Final Commit
    12/8/2020

---
+++ CS 18 Black Out Drew W/018_Challenge_Drew_W/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _018_Challenge_Drew_W
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ CS 18 Black Out Drew W/018_Challenge_Drew_W/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _018_Challenge_Drew_W
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public DateTime firstDate, secondDate;
+
+        //button
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SetBlackOutDates(firstDate, secondDate);
+
+            DateTime startTime = firstDate;
+            DateTime endTime = secondDate;
+
+            TimeSpan duration = endTime - startTime;
+
+            labelAnswer.Content = duration;
+        }
+
+        public void secondCalendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            secondDate = CalendarTwo.SelectedDate.Value;
+            answerLabelTwo.Content = secondDate;
+        }
+
+        public void firstCalendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            firstDate = calendarOne.SelectedDate.Value;
+            answerLabel.Content = firstDate;
+        }
+
+        private void SetBlackOutDates(DateTime firstDate, DateTime secondDate)
+        {
+            blackoutCalender.BlackoutDates.Add(new CalendarDateRange(firstDate, secondDate));
+        }
+    }
+}
